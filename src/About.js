c53e90f1e958c530be2e91b175680970a9e7e1c3
Daramola Oluwafemi <dfmopeoluwa@gmail.com>
2007-04-26 00:46:15 -0700

    about page added

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+import aboutImg from './images/about.jpg'
+function About() {
+    return (
+        <div className='about'>
+            <div className='about__Left'>
+                <h1>What is Tradesafegiftworld?</h1>
+                <p>Tradesafegiftworld is a platform for the future of funding that powering crypto for the new equity blockchain</p>
+            <div className='about__LeftContact'>
+                <button>Contact us</button>
+                <p> Watch Video</p>
+            </div>
+            </div>
+        <div className='about__Right'>
+            <img src={aboutImg} alt='about image'/>
+        </div>
+        </div>
+    )
+}
+
+export default About
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Main.css'
 import NavBar from './NavBar';
-
+import About from './About'
 function Main(){
     return (
         <div className='main'>
 <NavBar/>
+<About/>
        </div> 
     );
 
